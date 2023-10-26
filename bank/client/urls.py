1080c2719b37872792d5da50c0a7ea84631f6a09
Juan Dimas Abrante Hernandez <19jdah98@gmail.com>
2023-10-26 16:41:56 +0100

    Created the url for the login, change and reset passwords

--- bank/client/urls.py
+++ bank/client/urls.py
@@ -1,11 +1,5 @@
-from django.contrib.auth import views as auth_views
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
-    path('password-change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
-    path(
-        'passowrd-change/done',
-        auth_views.PasswordChangeDoneView.as_view(),
-        name='password_change_done',
-    ),
+    path('', include('django.contrib.auth.urls')),
 ]
