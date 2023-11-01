6227ad5cd5d574139ddb3ab108164e02ea51ed18
Juan Dimas Abrante Hernández <19jdah98@gmail.com>
2023-11-01 20:41:58 +0000

    Completed the models, views and templates for profile, registration, profile and user editing

--- bank/client/urls.py
+++ bank/client/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
     path('', views.dashboard, name='dashboard'),
+    path('register/', views.register, name='register'),
+    path('edit/', views.edit, name='edit'),
 ]
