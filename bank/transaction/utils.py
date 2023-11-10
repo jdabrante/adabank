4a7cf249a8aad67658025a4c7c8ededf05b2f70b
adrianhrb <adrian.herrera.br@gmail.com>
2023-11-10 15:15:04 +0000

    Added function to calclate taxes

--- bank/transaction/utils.py
+++ bank/transaction/utils.py
@@ -1,7 +1,15 @@
-from django.contrib.contenttypes.models import ContentType
+TAXES = {
+    'OUT': {'range1': 2, 'range2': 4, 'range3': 6},
+    'IN': {'range1': 1, 'range2': 2, 'range3': 3},
+    'PAY': {'range1': 2, 'range2': 4, 'range3': 6},
+}
 
-from .models import Transaction
 
-
-def create_transaction(agent, concept, timestamp, amount, kind, target):
-    target_ct = ContentType.objects.get_for_model(target)
+def calculate_taxes(transfer_kind: str, amount: int):
+    if amount < 50:
+        amount -= amount * (TAXES[transfer_kind]['range1'] % 100)
+    if 50 <= amount < 500:
+        amount -= amount * (TAXES[transfer_kind]['range2'] % 100)
+    if amount >= 500:
+        amount -= amount * (TAXES[transfer_kind]['range3'] % 100)
+    return amount
