ffd30fbe96409a18a76f1486ecaf36cbfbb3c567
Alejandro Hdez <alherdom@outlook.com>
2023-11-17 19:08:27 +0000

    Edited templates to DOR task

--- bank/account/views.py
+++ bank/account/views.py
@@ -6,10 +6,11 @@ from django.shortcuts import get_object_or_404, render
 
 from .forms import AccountCreationForm, CardCreationForm
 from .models import Account, Card, Status
+from transaction.models import Transaction
 from .utils import pin_generator
 
-CARD_CHAR_ID = 'C'
-ACCOUNT_CHAR_ID = 'A'
+CARD_CHAR_ID = "C"
+ACCOUNT_CHAR_ID = "A"
 
 
 @login_required
@@ -19,11 +20,15 @@ def create_account(request: HttpRequest) -> HttpResponse:
         if account_form.is_valid():
             cd = account_form.cleaned_data
             if request.user.check_password(cd["password"]):
-                new_account = Account.objects.create(client=request.user, alias=cd["alias"])
+                new_account = Account.objects.create(
+                    client=request.user, alias=cd["alias"]
+                )
                 new_account.code = f"A4-{new_account.id:04d}"
                 new_account.save()
                 # El create_done se tiene que cambiar
-                return render(request, "account/create_done.html", {"new_account": new_account})
+                return render(
+                    request, "account/create_done.html", {"new_account": new_account}
+                )
     account_form = AccountCreationForm()
     return render(request, "account/create.html", {"account_form": account_form})
 
@@ -31,7 +36,12 @@ def create_account(request: HttpRequest) -> HttpResponse:
 @login_required
 def account_list(request: HttpRequest) -> HttpResponse:
     accounts = Account.objects.filter(client=request.user)
-    return render(request, "account/list.html", {"accounts": accounts})
+    transactions = Transaction.objects.all()[:10]
+    return render(
+        request,
+        "account/list.html",
+        {"accounts": accounts, "transactions": transactions},
+    )
 
 
 @login_required
@@ -39,7 +49,9 @@ def account_detail(request: HttpRequest, account_id: int) -> HttpResponse:
     account = get_object_or_404(Account, id=account_id)
     transactions = account.transactions.all()[:10]
     return render(
-        request, "account/detail.html", {"account": account, "transactions": transactions}
+        request,
+        "account/detail.html",
+        {"account": account, "transactions": transactions},
     )
 
 
@@ -58,7 +70,9 @@ def card_create(request: HttpRequest, account_id) -> HttpResponse:
                 new_card.code = f"C4-{new_card.id:04d}"
                 new_card.save()
                 return render(
-                    request, "account/card/create_done.html", {"new_card": new_card, 'pin': pin}
+                    request,
+                    "account/card/create_done.html",
+                    {"new_card": new_card, "pin": pin},
                 )
     card_form = CardCreationForm()
     return render(request, "account/card/create_card.html", {"card_form": card_form})
@@ -66,7 +80,7 @@ def card_create(request: HttpRequest, account_id) -> HttpResponse:
 
 @login_required
 def card_list(request: HttpRequest) -> HttpResponse:
-    user_accounts = request.user.accounts.values_list('id', flat=True)
+    user_accounts = request.user.accounts.values_list("id", flat=True)
     cards = Card.objects.filter(account_id__in=user_accounts)
     return render(request, "account/card/list.html", {"cards": cards})
 
@@ -84,7 +98,7 @@ def change_status_card(request: HttpRequest, card_id: int) -> HttpResponse:
     card.save()
     return render(
         request,
-        'account/change_status.html',
+        "account/change_status.html",
         dict(
             card=card,
         ),
