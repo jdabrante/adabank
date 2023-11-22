bcf57c6000132b2d01e6b83cfc5f0325e96a01fe
alherdom <alherdom@outlook.com>
2023-11-22 23:56:09 +0000

    Edited templates
    Co-authored-by: Adrián Herrera <adrian.herrera.br@gmail.com>
    Co-authored-by: J.Dimas Abrante Hernández <jdabrante@users.noreply.github.com>

--- bank/account/utils.py
+++
@@ -1,7 +0,0 @@
-import random
-import string
-
-
-def pin_generator() -> str:
-    string_pin = string.digits + string.ascii_uppercase
-    return f"{random.choice(string_pin)}{random.choice(string_pin)}{random.choice(string_pin)}"
--- bank/transaction/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-
-class transferOutcomingForm(forms.Form):
-    sender = forms.CharField()
-    cac = forms.CharField()
-    concept = forms.CharField()
-    amount = forms.DecimalField()
