49b3189d199da4036b7af9d8436371554605b4f5
Juan Dimas Abrante Hernández <19jdah98@gmail.com>
2024-01-23 12:57:43 +0000

    Added some API views

--- bank/api/urls.py
+++ bank/api/urls.py
@@ -6,4 +6,10 @@ app_name = 'api'
 
 urlpatterns = [
     path('accounts/', views.AccountListView.as_view(), name='api_ccount_list'),
+    path('accounts/<pk>/', views.AccountDetailView.as_view(), name='api_ccount_detail'),
+    path('transactions/', views.TransactionListView.as_view(), name='api_transaction_list'),
+    path('transactions/<pk>/', views.TransactionDetailView.as_view(), name='api_transaction_detail'),
+    path('cards/',views.CardListView.as_view(), name='api_card_list' ),
+    path('cards/<pk>/', views.CardDetailtView.as_view(), name='api_card_detail'),
+
 ]
--- bank/api/views.py
+++ bank/api/views.py
@@ -1,10 +1,71 @@
 from rest_framework import generics
-
-from account.models import Account
-
-from .serializers import AccountSerializer
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+from account.models import Account, Card
+from transaction.models import Transaction
+from rest_framework.response import Response
+from .serializers import AccountSerializer, TransactionSerializer, CardSerializer
 
 
 class AccountListView(generics.ListAPIView):
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
     queryset = Account.objects.all()
     serializer_class = AccountSerializer
+    def get(self, request):
+        queryset = self.get_queryset().filter(client__username=request.user.username)
+        serializer = AccountSerializer(queryset, many=True)
+        return Response(serializer.data)
+    
+class AccountDetailView(generics.RetrieveAPIView):
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer
+    # Check this...
+    def get(self, request, pk):
+        queryset = self.get_queryset().filter(client__username=request.user.username, code=pk)
+        serializer = AccountSerializer(queryset, many=True)
+        return Response(serializer.data)
+    
+class TransactionListView(generics.ListAPIView):
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+    queryset = Transaction.objects.all()
+    serializer_class = TransactionSerializer
+    def get(self, request):
+        queryset = self.get_queryset().filter(account__client__username=request.user.username)
+        serializer = TransactionSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+class TransactionDetailView(generics.RetrieveAPIView):
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+    queryset = Transaction.objects.all()
+    serializer_class = TransactionSerializer
+    #Check this...
+    def get(self, request, pk):
+        queryset = self.get_queryset().filter(account__client__username=request.user.username, pk=pk)
+        serializer = AccountSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+class CardListView(generics.ListAPIView):
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+    queryset = Card.objects.all()
+    serializer_class = CardSerializer
+    def get(self, request):
+        queryset = self.get_queryset().filter(account__client__username=request.user.username)
+        serializer = CardSerializer(queryset, many=True)
+        return Response(serializer.data)
+    
+
+class CardDetailtView(generics.RetrieveAPIView):
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+    queryset = Card.objects.all()
+    serializer_class = CardSerializer
+    def get(self, request, pk):
+        queryset = self.get_queryset().filter(account__client__username=request.user.username, code=pk)
+        serializer = CardSerializer(queryset, many=True)
+        return Response(serializer.data)
