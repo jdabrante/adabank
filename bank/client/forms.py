a0fa14af70e4ff8e24fc77ba7c084d3a6f4703a1
adrianhrb <adrian.herrera.br@gmail.com>
2024-02-07 15:37:18 +0000

    Some things with gettext lazy added

--- bank/client/forms.py
+++ bank/client/forms.py
@@ -1,64 +1,66 @@
 from django import forms
 from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
+
 from .models import Profile
 
 
 class UserRegistrationForm(forms.ModelForm):
     password = forms.CharField(
-        label="", widget=forms.PasswordInput(attrs={"placeholder": "Password"})
+        label='', widget=forms.PasswordInput(attrs={'placeholder': _('Password')})
     )
     password2 = forms.CharField(
-        label="", widget=forms.PasswordInput(attrs={"placeholder": "Re-type Password"})
+        label='', widget=forms.PasswordInput(attrs={'placeholder': _('Re-type Password')})
     )
     username = forms.CharField(
-        label="",
-        widget=forms.TextInput(attrs={"placeholder": "Username"}),
-        help_text="",
+        label='',
+        widget=forms.TextInput(attrs={'placeholder': _('Username')}),
+        help_text='',
     )
     first_name = forms.CharField(
-        label="",
-        widget=forms.TextInput(attrs={"placeholder": "First Name"}),
-        help_text="",
+        label='',
+        widget=forms.TextInput(attrs={'placeholder': _('First Name')}),
+        help_text='',
     )
     last_name = forms.CharField(
-        label="",
-        widget=forms.TextInput(attrs={"placeholder": "Last Name"}),
-        help_text="",
+        label='',
+        widget=forms.TextInput(attrs={'placeholder': _('Last Name')}),
+        help_text='',
     )
     email = forms.EmailField(
-        label="", widget=forms.EmailInput(attrs={"placeholder": "Email"}), help_text=""
+        label='', widget=forms.EmailInput(attrs={'placeholder': _('Email')}), help_text=''
     )
 
     class Meta:
         model = User
-        fields = ["username", "first_name", "last_name", "email"]
+        fields = ['username', 'first_name', 'last_name', 'email']
 
     def clean_password2(self):
         cd = self.cleaned_data
-        if cd["password"] != cd["password2"]:
+        if cd['password'] != cd['password2']:
             raise forms.ValidationError("Passwords don't match.")
-        return cd["password2"]
+        return cd['password2']
+
 
 class ProfileRegistrationForm(forms.ModelForm):
-    date_of_birth = forms.DateField(
-        label="", widget=forms.TextInput(attrs={"type": "date"})
-    )
+    date_of_birth = forms.DateField(label='', widget=forms.TextInput(attrs={'type': _('date')}))
     identification = forms.CharField(
-        label="", widget=forms.TextInput(attrs={"placeholder": "Identification"})
+        label='', widget=forms.TextInput(attrs={'placeholder': _('Identification')})
     )
 
     class Meta:
         model = Profile
-        fields = ["identification"]
+        fields = ['identification']
 
 
 class ProfileEditForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ["date_of_birth", "avatar"]
+        fields = ['date_of_birth', 'avatar']
+        labels = {'date_of_birth': _('Date of birth'), 'avatar': _('avatar')}
 
 
 class UserEditForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ["first_name", "last_name", "email"]
+        fields = ['first_name', 'last_name', 'email']
