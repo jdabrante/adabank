678be048b91d42a4d5d2a6c0e013c39916493f1a
adrianhrb <adrian.herrera.br@gmail.com>
2024-02-15 15:37:21 +0000

    Changes on index, logout button...

--- bank/account/views.py
+++ bank/account/views.py
@@ -72,7 +72,7 @@ def card_create(request: HttpRequest, account_id: int) -> HttpResponse:
     }
     message = _(
         'Dear %(user)s\nThis is your PIN for the new requested card: %(card)s\nNEVER FORGET IT, IT WILL ONLY BE SHOWN NOW'
-    ) % {'user': request.user.username, 'card': new_card.pin}
+    ) % {'user': request.user.username, 'card': pin}
     send_mail(subject, message, 'adalovelacebank@gmail.com', [request.user.email])
     return render(
         request,
