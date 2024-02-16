9c6bfa8027b94a7c119891d5c4e021b29c4a3804
adrianhrb <adrian.herrera.br@gmail.com>
2024-02-16 14:49:13 +0000

    Api problems solved

--- bank/api/serializers.py
+++ bank/api/serializers.py
@@ -3,18 +3,30 @@ from rest_framework import serializers
 from account.models import Account, Card
 from transaction.models import Transaction
 
+
 class AccountSerializer(serializers.ModelSerializer):
     class Meta:
         model = Account
-        fields = ['client', 'code', 'alias', 'status', 'balance']
+        fields = ['id', 'client', 'code', 'alias', 'status', 'balance']
+
 
 class TransactionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Transaction
-        fields = ['agent', 'concept', 'timestamp', 'amount', 'kind', 'account', 'card', 'commission']
+        fields = [
+            'id',
+            'agent',
+            'concept',
+            'timestamp',
+            'amount',
+            'kind',
+            'account',
+            'card',
+            'commission',
+        ]
+
 
 class CardSerializer(serializers.ModelSerializer):
     class Meta:
         model = Card
-        fields = ['account', 'code', 'alias', 'status', 'expiry']
-
+        fields = ['id', 'account', 'code', 'alias', 'status', 'expiry']
